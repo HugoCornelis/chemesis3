357db9729f61b9f3e5d943a6b1f3e3069e5fe6d7
Hugo Cornelis <hugo.cornelis@gmail.com>
2011-06-26 11:22:21 +0000

    1. Added core integration routine with support for forward euler,    trapezoid and runge-kutta4 integration.  The definition of some of    the may be missing, or maybe they are not needed, will see.

--- solver.c
+++ solver.c
@@ -145,7 +145,7 @@ int Chemesis3Initiate(struct simobj_Chemesis3 *pch3)
 
 int Chemesis3SingleStep(struct simobj_Chemesis3 *pch3)
 {
-    //- set default result : ok
+    //- set default result: ok
 
     int iResult = TRUE;
 
@@ -160,6 +160,94 @@ int Chemesis3SingleStep(struct simobj_Chemesis3 *pch3)
     //- simulate the pools
 
     iResult = iResult && Chemesis3SingleStepPools(pch3);
+
+    //- return result
+
+    return(iResult);
+}
+
+
+int Chemesis3SingleStepPools(struct simobj_Chemesis3 *pch3)
+{
+    //- set default result: ok
+
+    int iResult = TRUE;
+
+    //- loop over all pools
+
+    int iPool;
+
+    for (iPool = 0 ; iPool < pch3->iPools ; iPool++)
+    {
+	struct ch3_pool *ppool = &pch3->ppool[iPool];
+
+	//- initialize
+
+	double dA = 0;
+
+	double dAConc = 0;
+
+	double dB = 0;
+
+	double dAMoles = 0;
+
+	//- loop over all reactions attached to this pool
+
+	int iReaction;
+
+	for (int iReaction = 0 ; iReaction < ppool->iReactions ; iReaction++)
+	{
+	    //- retrieve the index of this reaction
+
+	    int iIndex = ppool->piReactions[iReaction];
+
+	    //- get forward and backward rates of this reaction
+
+	    struct ch3_reaction *preaction = pch3->preaction[iIndex];
+
+	    double dForwardSolved = preaction->dForwardSolved;
+
+	    double dBackwardSolved = preaction->dBackwardSolved;
+
+	    //- connect the pool equation with the reaction equation
+
+	    dAConc += dForwardSolved;
+
+	    if (ppool->dConcentration != 0)
+	    {
+		dB += dBackwardSolved / ppool->dConcentration;
+	    }
+
+	    //- integrate to the next time step
+
+	    dA = dAMoles / ppool->dVolume / (AVOGADRO * ppool->dUnits) + dAConc;
+
+	    ppool->dConcentration
+		= IntegrateMethod
+		  (
+		      (dB < 1.0e-10
+		       ? FEULER_INT
+		       : EEULER_INT),
+		      /* ppool */ NULL,
+		      ppool->dConcentration,
+		      dA,
+		      dB,
+		      pch3->dStep,
+		      "concentration");
+
+	    //- check for parameterized minimum concentration boundary
+
+	    if (ppool->dConcentration < ppool->dMinimum)
+	    {
+		ppool->dConcentration = ppool->dMinimum;
+	    }
+
+	    //- calculate the total number of ions in the pool
+
+	    ppool->dQuantity = ppool->dConcentration * ppool->dVolume * AVOGADRO * ppool->dUnits;
+
+	}
+    }
 
     //- return result
 
@@ -217,3 +305,119 @@ int Chemesis3SingleStepReactions(struct simobj_Chemesis3 *pch3)
 }
 
 
+static
+double IntegrateMethod
+(int method, void *pv, double state, double A, double B, double dt, char *name)
+{
+    double D;
+    double result;
+
+    switch(method){
+    case EEULER_INT:
+	/*
+	** the default is the exponential integration form
+	*/
+	D = exp(-B*dt);
+	result = state*D + (A/B)*(1-D);
+	break;
+    case GEAR_INT:
+	/*
+	** Gear 2nd order
+	*/
+	/* Attach buffer below segment, instead of to parent (BPG 15-5-91)
+	   result = Gear(segment->parent,&(segment->child),*/
+	result = Gear(segment,
+		      state,A,B,dt,name);
+	break;
+    case AB2_INT:
+	/*
+	** Adams-Bashforth 2 step
+	*/
+	/* Attach buffer below segment, instead of to parent (BPG 15-5-91)
+	   result = AB2(segment->parent,&(segment->child),*/
+	result = AB2(segment, state,A - state*B,dt, name);
+	break;
+    case AB3_INT:
+	/*
+	** Adams-Bashforth 3 step
+	*/
+	/* Attach buffer below segment, instead of to parent (BPG 15-5-91)
+	   result = AB3(segment->parent,&(segment->child),*/
+	result = AB3(segment, state,A - state*B,dt, name);
+	break;
+    case TRAPEZOIDAL_INT:
+	/*
+	** Trapezoidal
+	*/
+#define Trapezoid(y,A,B,dt) \
+	(((y)*(1-(dt)*(B)/2.0)+(dt)*(A))/(1+(dt)*(B)/2.0))
+
+	result = Trapezoid(state,A,B,dt);
+	break;
+	/* Why not include the 4 and 5 step methods ? (BPG 22-5-91) */
+    case AB4_INT:
+	/*
+	** Adams-Bashforth 4 step
+	*/
+	/* Attach buffer below segment, instead of to parent (BPG 15-5-91)
+	   result = AB4(segment->parent,&(segment->child),*/
+	result = AB4(segment, state,A - state*B,dt, name);
+	break;
+    case AB5_INT:
+	/*
+	** Adams-Bashforth 5 step
+	*/
+	/* Attach buffer below segment, instead of to parent (BPG 15-5-91)
+	   result = AB5(segment->parent,&(segment->child),*/
+	result = AB5(segment, state,A - state*B,dt, name);
+	break;
+	/* The oldest and greatest ! (BPG 24-5-91) */
+    case RK_INT:
+	/*
+	** Runge-Kutta 4 step
+	*/
+	result = Runge_Kutta(state,A,B,dt);
+	break;
+    case FEULER_INT:
+	/*
+	** Euler: a definite must
+	*/
+#define Euler(y,dy,dt) 		((y) + (dy)*(dt))
+	result = Euler(state,A - state*B,dt);
+	break;
+	/* A predictor-corrector method (BPG 22-5-91) */
+    case EPC_INT:
+	/*
+	** Euler predictor - modified Euler corrector
+	(from Parnas and Segev, J.Physiol. (1979) 295:323-343)
+	*/
+	result = Euler_Predictor_Corrector(state,A,B,dt);
+	break;
+    default :
+	/* 
+	** default to good old Exp Euler
+	*/
+	D = exp(-B*dt);
+	result = state*D + (A/B)*(1-D);
+	break;
+    }
+    return(result);
+}
+
+
+/* Runge-Kutta 4-step method (BPG 22-5-91) 
+*/
+static double Runge_Kutta(double y, double A, double B, double dt)
+{
+    double k1, k2, k3, k4, value;
+
+    k1 = (A - B*y) * dt;
+    k2 = (A - B*(y + k1/2)) * dt;
+    k3 = (A - B*(y + k2/2)) * dt;
+    k4 = (A - B*(y + k3)) * dt;
+
+    value = y + (k1 + 2*k2 + 2*k3 + k4) / 6;
+
+    return(value);
+
+}
