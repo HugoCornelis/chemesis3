71c46367ba322f14f69c06ee9d657c97fec53694
mandorodriguez <mandorodriguez@gmail.com>
2011-07-13 21:55:34 +0000

    1. Fixed a bad rule in the mtn-ignore file.
    2. Fixed the path used in the python test library. 
    
    3. Added an import test script to test the import of the compiled swig library.

--- glue/swig/python/neurospaces/chemesis3/__init__.py
+++ glue/swig/python/neurospaces/chemesis3/__init__.py
@@ -1,16 +1,34 @@
 """!
-@package chemesis3
-
+@package chemesis3 Kinetic reaction and biochemical reaction pathways for G3.
 
 
 """
 
+# System imports
+import os
+import pdb
+import sys
+
+try:
+
+    import chemesis3_base
+
+except ImportError, e:
+
+    sys.exit("Could not import compiled SWIG chemesis3 library: %s" % e)
+
+
+import __cbi__
+
+
+#************************* Begin Heccer **************************
 class Chemesis3:
 
 
 
-    def __init__(self):
+    def __init__(self, name="Untitled Chemesis3"):
 
         pass
 
 
+#**************************** End Heccer **************************
---
+++ tests/python/test_library.py
@@ -0,0 +1,81 @@
+"""
+A test library of functions and classes
+for running tests from python.
+
+"""
+
+import os
+import sys
+import pdb
+
+
+INITIATE=1
+INTERVAL_START=-0.1
+INTERVAL_END=0.05
+INTERVAL_ENTRIES=3000
+INTERPOL_INTERVAL_ENTRIES=149
+OPTIONS=0
+OUTPUT=0
+REPORTING_GRANULARITY=1
+SCHEDULE=1
+STEPS=10
+TIME_GRANULARITY=1e-9
+TIME_STEP=2e-5
+
+# This is borrowed from django's setup tools
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+def add_package_path(package):
+    """
+    Adds an import path to a python module in a project directory.
+    """
+
+    path = os.path.join(os.environ['HOME'],
+                        'neurospaces_project',
+                        package,
+                        'source',
+                        'snapshots',
+                        '0',
+                        'glue',
+                        'swig',
+                        'python')
+
+
+    build_dir = os.path.join(path, 'build')
+
+    python_build = ""
+    parts = []
+    
+    if os.path.exists(build_dir):
+
+        for curr_dir, directories, files in os.walk( build_dir ):
+
+            if os.path.isfile( os.path.join( curr_dir, '__cbi__.py' )):
+
+                parts = list(fullsplit(curr_dir))
+
+                # remove the module and neurospaces directory
+                parts.pop()
+
+                python_build = os.path.join(os.sep, os.path.join(*parts))
+                sys.path.append(python_build)
+
+    # Add paths
+    sys.path.append(path)
+
+
+
+    
